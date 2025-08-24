07539b905363cb3447bb7c639107bc883e23bf5b
Hrishabh Joshi <hrishabhjoshi123@gmail.com>
2025-08-24 22:11:56 +0530

    Fixed analytic

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,16 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
-import { Analytics } from "@vercel/analytics/react"
+import {Analytics} from "@vercel/analytics/react";
 
 const queryClient = new QueryClient();
 
 const App = () => (
     <QueryClientProvider client={queryClient}>
+        <Analytics />
             <BrowserRouter>
                 <Routes>
-                    <Analytics />
+
                     <Route path="/" element={<Index />} />
                     {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
                     <Route path="*" element={<NotFound />} />
