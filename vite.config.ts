b8444fe7c26ba27ecda935f4b4ddbfa2039e079c
Hrishabh Joshi <hrishabhjoshi123@gmail.com>
2025-08-24 15:41:33 +0530

    SEO optimizations

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,16 @@
 import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
-
+import ViteSitemap from "vite-plugin-sitemap"
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react()],
-    resolve: {
+    plugins: [
+        react(),
+        ViteSitemap({
+            hostname: "https://docs.codeexeengine.com", // replace with your domain
+            dynamicRoutes: ["/"], // add important docs routes
+        }),
+    ],    resolve: {
         alias: {
             "@": path.resolve(__dirname, "./src"),
         },
